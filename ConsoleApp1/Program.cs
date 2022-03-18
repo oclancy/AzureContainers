d68582b4ff8a93fef0559fc76bf08421bfb807a5
Oliver Clancy <oliver.clancy@firmus-software.co.uk>
2022-03-18 11:55:07 +0000

    Setup serilog and di

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,60 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Autofac.Extensions.DependencyInjection;
 
-using Firmus.Protobuf.Messages;
+using Firmus.Common;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-using Polly;
+using Microsoft.Extensions.Options;
 
 using Serilog;
+using Serilog.Events;
 
 using ServiceStack.Redis;
 
-var redisClientManager = new PooledRedisClientManager("redis:6379");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 
-using IHost host = Host.CreateDefaultBuilder(args)
+
+try
+{
+
+
+    using IHost host = Host.CreateDefaultBuilder(args)
                         .UseSerilog()
                         .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                        .ConfigureServices((_, services) =>
+                        .ConfigureServices((context, services) =>
                         {
+                            var configurationRoot = context.Configuration;
+
                             services
-                            .AddSingleton<IRedisClientsManager>(redisClientManager)
-                            .AddTransient<Publisher>();
+                            .AddSingleton<IRedisClientsManager, PooledRedisClientManager>((sp) =>
+                            {
+                                var options = sp.GetService<IOptions<RedisOptions>>().Value;
+                                return new($"{options.IpAddress}:{options.Port}");
+                            })
+                            .Configure<RedisOptions>(
+                                configurationRoot.GetSection(nameof(RedisOptions)))
+                            .AddHostedService<Publisher>();
                         })
                         .Build();
 
-using IServiceScope serviceScope = host.Services.CreateScope();
-IServiceProvider provider = serviceScope.ServiceProvider;
+    await host.RunAsync();
 
-
-var publisher = provider.GetRequiredService<Publisher>();
-
-
-while (true)
-{
-    try
-    {
-        var person = new Person();
-        person.Name = "Ollie";
-
-        Policy
-            .Handle<Exception>()
-            .Retry()
-            .Execute(() => publisher.Publish(person, "people"));
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-
-    await Task.Delay(1000);
 }
-
-
-await host.RunAsync();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 
 
--- Firmus.Common/RedisOptions.cs
+++ Firmus.Common/RedisOptions.cs
@@ -2,6 +2,8 @@
 {
     public class RedisOptions
     {
+        public string IpAddress { get; set; } = "127.0.0.1";
+        public int Port { get; set; } = 6379;
 
     }
 }
